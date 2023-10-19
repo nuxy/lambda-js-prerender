666ccd39e74aa9cc91d9ba10d53b68c8a19cc7f7
Marc S. Brooks <marc@mbrooks.info>
2023-10-19 11:31:33 -0700

    Updated servers.url to INPUT type

--- src/openapi.js
+++ src/openapi.js
@@ -6,6 +6,11 @@ module.exports = {
     version: process.env.npm_package_version
   },
   servers: [{
-    url: 'http://localhost:3000'
+    url: '{FunctionUrl}',
+    variables: {
+      FunctionUrl: {
+        default: 'http://localhost:3000'
+      }
+    }
   }]
 };
